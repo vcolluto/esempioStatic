b97e50dd4f58abfafb8f556e83230ec5e2ada025
Vincenzo Colluto <enzocoll@tiscali.it>
2023-12-18 12:41:19 +0100

    aggiunti esempi su autoincremento e date

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -1,5 +1,9 @@
 package org.generation.italy;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -14,9 +18,10 @@ public class Main {
 		t1.altezza=20;
 		
 		System.out.println("t1:");
+		System.out.println("Id: "+t1.getId());
 		System.out.println("Base: "+t1.base);
 		System.out.println("Altezza: "+t1.altezza);
-		System.out.println("Numero triangoli: "+Triangolo.getNumeroTriangoli());		
+		//System.out.println("Numero triangoli: "+Triangolo.getNumeroTriangoli());		
 	
 		
 		
@@ -25,9 +30,14 @@ public class Main {
 		t2.altezza=15;		
 		
 		System.out.println("\nt2:");
+		System.out.println("Id: "+t2.getId());
 		System.out.println("Base: "+t2.base);
 		System.out.println("Altezza: "+t2.altezza);
+		
 		System.out.println("Numero triangoli: "+Triangolo.getNumeroTriangoli());
+		
+		
+		
 		
 		
 		System.out.println("La somma dei numeri 3 e 5 è: "+Calcolatrice.somma(3, 5));  //non ho fatto nessun new (perché il metodo è statico)
@@ -40,6 +50,11 @@ public class Main {
 	///	System.out.println("Calcolatrice 2 - batteria: "+Calcolatrice.getLivelloBatteria());  //non si può fare perché il livello batteria dipende dall'istanza (il metodo non è statico)
 		
 	
+		LocalDate d1=LocalDate.now(); 		//assegno a d1 il valore della data attuale
+		
+		System.out.println("La data attuale (formato standard) è: "+ d1);  //formato standard
+		DateTimeFormatter df=DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+		System.out.println("La data attuale (formato ita) è : "+ d1.format(df));
 	}
 
 }
--- src/org/generation/italy/Triangolo.java
+++ src/org/generation/italy/Triangolo.java
@@ -4,16 +4,24 @@ public class Triangolo {
 	float base,altezza;					//ogni triangolo ha la sua base e la sua altezza
 	
 	private static int numeroTriangoli=0;			//condiviso da tutti i triangoli
-
+    private int id;		//attributo di istanza
 	
 	
 	public Triangolo() {
 		numeroTriangoli++;		//ogni volta che si crea un triangolo incremento di 1
+		id=numeroTriangoli;
 	}
 
 
 	public static int getNumeroTriangoli() {
 		return numeroTriangoli;
 	}
+
+
+	public int getId() {
+		return id;
+	}
+	
+	
 
 }
